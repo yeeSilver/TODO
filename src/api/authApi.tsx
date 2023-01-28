866125b3a162e553a804ef262a17715dcee279da
yeeun_An <an446@naver.com>
2023-01-28 17:08:07 +0900

    :lipstick: global style add, font assets add

--- src/api/authApi.tsx
+++
@@ -1,5 +0,0 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
-import { baseURL } from "../constants/api";
-
-//axios instance 생성
-export const apiRequest = axios.create({ baseURL });
