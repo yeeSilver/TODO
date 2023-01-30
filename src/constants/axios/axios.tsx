5a22fefaf340d85e32123d3313362145fe5d09cb
yeeun_An <an446@naver.com>
2023-01-30 18:23:36 +0900

    :sparkles: create todo modal & axios interceptor 수정

--- src/constants/axios/axios.tsx
+++ src/constants/axios/axios.tsx
@@ -1,5 +1,6 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
-import { config } from "process";
+import { useRef } from "react";
+
 import { useNavigate } from "react-router-dom";
 import { api } from "../../utils/instance";
 import token from "../../utils/localStorage";
@@ -9,19 +10,29 @@ import { ACCESS_TOKEN_KEY, REQUEST_KEY } from "../token";
 
 export const baseURL: string = process.env.REACT_APP_BASE_URL as string;
 
+const client = axios.create({ baseURL });
+
 interface IIntError {
   details: string;
 }
 
+interface ITodoResponse {
+  title: string;
+  content: string;
+  id: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
 //요청 타임아웃 설정
-api.defaults.timeout = 2500;
+// client.defaults.timeout = 2500;
 
 //요청 인터셉터 추가
-api.interceptors.request.use(
+client.interceptors.request.use(
   async (config: AxiosRequestConfig) => {
-    config.headers = config.headers ?? {};
+    // config.headers = config.headers ?? {};
     //요청을 보내기 전에 수행할 로직
     const accessToken = token.getToken(ACCESS_TOKEN_KEY);
+    console.log(accessToken);
     if (accessToken) {
       config.headers.Authorization = accessToken;
     }
@@ -35,7 +46,7 @@ api.interceptors.request.use(
 );
 
 //응답 인터셉터 추가
-api.interceptors.response.use(
+client.interceptors.response.use(
   (response: AxiosResponse) => {
     //응답에 대한 로직 작성
     const res = response.data;
@@ -45,10 +56,12 @@ api.interceptors.response.use(
   (error: AxiosError<IIntError>) => {
     //응답 에러가 발생했을 때 수행할 로직
     // const isTokenAvaliable = error.response?.data.details === "Token is missing"
-    console.log(error); //디버깅
+    console.log(error.response?.data.details);
+    // console.log(error);
     alert("토큰이 유효하지 않아, 로그인 페이지로 돌아갑니다.");
-    const navigate = useNavigate();
-    navigate(`/auth`);
+
     return Promise.reject(error);
   }
 );
+
+export default client;
---
+++ src/hooks/mutation/todo/useCreateTodo.tsx
@@ -0,0 +1,60 @@
+import { AxiosResponse } from "axios";
+import { useMutation } from "@tanstack/react-query";
+import { TodoAPI } from "../../../constants/api/api";
+import client from "../../../constants/axios/axios";
+import { useNavigate } from "react-router-dom";
+import { api } from "../../../utils/instance";
+import { ACCESS_TOKEN_KEY } from "../../../constants/token";
+import token from "../../../utils/localStorage";
+import { REQUEST_KEY } from "../../../constants/token";
+
+interface ITodoForm {
+  title: string;
+  content: string;
+}
+
+interface ITodoResponse {
+  title: string;
+  content: string;
+  id: string;
+  createAt: string;
+  updatedAt: string;
+}
+
+interface ICreateTodo {
+  data: ITodoResponse;
+}
+
+const useCreateTodo = () => {
+  const navigate = useNavigate();
+  const accessToken = token.getToken(ACCESS_TOKEN_KEY);
+  console.log(accessToken);
+  //분리해야 할 듯
+  const createRequest = ({ title, content }: ITodoForm) =>
+    client.post<ICreateTodo>(
+      `${TodoAPI.CREATE}`,
+      {
+        title: title,
+        content: content,
+      }
+      // {
+      //   headers: {
+      //     Authorization: accessToken,
+      //   },
+      // }
+    );
+
+  return useMutation(createRequest, {
+    onSuccess: (data: AxiosResponse<ICreateTodo>) => {
+      // navigate(`/todos/${data.data.id}`);
+      console.log(data);
+    },
+    onError: (error) => {
+      alert(error);
+      navigate(`/auth`);
+      console.log(error);
+    },
+  });
+};
+
+export default useCreateTodo;
