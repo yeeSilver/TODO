bcfee6f70a5592de17235c37f9e521fdd9ce40f9
yeeun_An <an446@naver.com>
2023-01-30 18:25:26 +0900

    :recycle:token 저장할 떄 key 이름 변경

--- src/hooks/mutation/auth/useLogin.tsx
+++ src/hooks/mutation/auth/useLogin.tsx
@@ -4,6 +4,9 @@ import { useMutation } from "@tanstack/react-query";
 import { AuthAPI } from "../../../constants/api/api";
 import token from "../../../utils/localStorage";
 import { api } from "../../../utils/instance";
+import { useNavigate } from "react-router-dom";
+import client from "../../../constants/axios/axios";
+import { ACCESS_TOKEN_KEY } from "../../../constants/token";
 
 interface IForm {
   inputEmail: string;
@@ -18,6 +21,7 @@ interface ILoginResponse {
 
 const useLogin = () => {
   //분리해야 할 듯
+  const navigate = useNavigate();
   const loginRequest = ({ inputEmail, inputPw }: IForm) =>
     api.post(`${AuthAPI.LOGIN}`, {
       email: inputEmail,
@@ -26,8 +30,8 @@ const useLogin = () => {
 
   return useMutation(loginRequest, {
     onSuccess: (loginData: AxiosResponse<ILoginResponse>) => {
-      token.setToken("token", loginData.data.token);
-      console.log("success");
+      token.setToken(ACCESS_TOKEN_KEY, loginData.data.token);
+      navigate(`/todo`);
     },
     onError: (error) => {
       console.log(error);
